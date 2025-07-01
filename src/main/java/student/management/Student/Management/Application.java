f1489feef2292278ebbb8d856764633fd4f0196f
Keisuke Konishi <kkd28mr@gmail.com>
2025-07-01 14:31:37 +0900

    Apache Commons Langを追加

--- src/main/java/student/management/Student/Management/Application.java
+++ src/main/java/student/management/Student/Management/Application.java
@@ -1,5 +1,6 @@
 package student.management.Student.Management;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
